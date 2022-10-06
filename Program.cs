bef970b00c1f8ea2608e606ffbc1d57016623f22
CryingCoder <112829574+CryingCoder@users.noreply.github.com>
2022-10-06 17:28:00 -0400

    Committed

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿namespace L8ClassAndObj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Students s = new Students("Rone", 33);
+
+            //PRINT OUT THE DATA TYPE ABOVE WITH THE PARAMETERS SUPPLIED
+            //Console.WriteLine(s);
+
+            //LIST INITIALIZED FOR CAR CLASS
+            List<Students> students = new List<Students>();
+            
+
+            students.Add(new Students("Jimmy", 13));
+            students.Add(new Students("Hannah Banana", 21));
+            students.Add(new Students("Justin", 30));
+            students.Add(new Students("Sarah", 53));
+            students.Add(new Students("Hannibal", 15));
+            students.Add(new Students("Phillip", 16));
+            students.Add(new Students("Maria", 63));
+            students.Add(new Students("Abe", 33));
+            students.Add(new Students("Curtis", 10));
+
+
+            //LINQ QUERIES TO CHECK ALL CONDITIONS AGAINST LIST
+            //DrinkingAge
+            List<Students> drinkingAge = students.Where(s => s.StudentAge >= 21).ToList();
+            PrintStudents(drinkingAge);
+            Console.WriteLine("------------");//FORMATTING
+
+            //Oldest Student
+            int oldStudent = students.Max(s => s.StudentAge);
+            List<Students> oldestStudent = students.Where(s => s.StudentAge == oldStudent).ToList();
+            PrintStudents(oldestStudent);
+            Console.WriteLine("------------");//FORMATTING
+
+            //Youngest Student
+            List<Students> youngestStudent = students.Where(s => s.StudentAge == oldStudent).ToList();
+            int youngStudent = students.Min(s => s.StudentAge);
+            PrintStudents(youngestStudent);
+            Console.WriteLine("------------");//FORMATTING
+
+            //Oldest under 25
+            List<Students> under25 = students.Where(s => s.StudentAge < 25).ToList();
+            int max25 = under25.Max(s => s.StudentAge);
+            List<Students> max25L = under25.Where(s => s.StudentAge == max25).ToList();
+            PrintStudents(max25L);
+            Console.WriteLine("------------");//FORMATTING
+
+            //Over 21 and age is even
+            List<Students> above21 = students.Where(s => s.StudentAge > 21 && s.StudentAge % 2 == 0).ToList();
+            PrintStudents(above21);
+            Console.WriteLine("------------");//FORMATTING
+
+            //Teenage Studens (13-19 Inclusive)
+            List<Students> teenage = students.Where(s => s.StudentAge <= 19 && s.StudentAge >= 13).ToList();
+            PrintStudents(teenage);
+            Console.WriteLine("------------");//FORMATTING
+
+            //Name starts with vowel
+            char[] vowel = { 'A', 'E', 'I', 'O', 'U' };
+            List<Students> vowels = students.Where(s => vowel.Contains(s.Student.First())).ToList();
+            PrintStudents(vowels);
+            Console.WriteLine("------------");//FORMATTING
+
+            //Order by age ascending
+            List<Students> ascending = students.OrderByDescending(s => s.Student).ToList();
+            PrintStudents(ascending);
+            Console.WriteLine("------------");//FORMATTING
+
+
+        }
+
+        public static void PrintStudents(List<Students> pupil)
+        {
+            for (int i = 0; i < pupil.Count; i++)
+            {
+                Students s = pupil[i];
+                Console.WriteLine($"Student:{s.Student} Age:{s.StudentAge}\n");
+            }
+        }
+    }
+}
---
+++ Students.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace L8ClassAndObj
+{
+    public class Students
+    {
+        //PROPERTIES - TRACKING PRICE, MAKE, MODEL, COLOR, YEAR
+
+        //CONSTRUCTOR - USE TO FILL IN THE PROPERTIES
+
+        //METHODS - NOT CURRENTLY NEEDED - WE WON'T NEED TO PERFORM AN ACTION
+
+
+
+        //BELOW PROPERTIES - get; set; ALLOWS US TO ALTER THE PROPERTIES AS THE CLASS IS RUNNING
+        public string Student { get; set; }
+        public int StudentAge { get; set; }
+
+        //public string Color { get; set; }
+        //public int Year { get; set; }
+        //public double MilesTraveled { get; set; }
+
+
+        //THIS IS A CONSTRUCTOR - CONSTRUCTORS ARE METHODS WITHOUT A RETURN TYPE OR "STATIC" IN THE SIGNATURE
+        //CONSTRUCTOR BUILDS AND RETURNS AN INSTANCE(OBJECT) OF THE MATCHING CLASS
+        //IN ORDER TO TELL C# I AM MAKING A CAR CONSTRUCTOR - THE CONSTRUCTOR NAME IN THE SIGNATURE must MATCH THE NAME OF THE CLASS.
+        public Students(string Student, int StudentAge)
+        //WHEN PARAMETERS ARE LISTED IN THE CONSTRUCTOR I AM REQUIRING THESE INPUTS FOR THIS OBJECT
+        //WHEN THE PARAMETER MATCHES THE PROPERTY NAME, 
+        {
+            //USING "this.Property" GIVES OUR PROPERTY THE INITIALIZED DATA ON THE RIGHT OF THE EQUALS
+            this.Student = Student;
+            this.StudentAge = StudentAge;
+            //this.Color = Color;
+            //this.Year = Year;
+        }
+
+        /*//PRACTICING METHODS WITH CLASSES - NOTICE THE METHOD IN THE CLASS LACKS "static"
+        public void Drive()
+        {
+            MilesTraveled++;
+        }*/
+    }
+}
